cd406b872b16249575925d50745579d7a71f29eb
吕发强 <903019642@qq.com>
2017-07-21 09:14:26 +0800

    修改ToastUtil ,添加 自定义Toast View 的方法。

--- library/src/main/java/com/lvfq/library/utils/LvToastUtil.java
+++ library/src/main/java/com/lvfq/library/utils/LvToastUtil.java
@@ -2,6 +2,7 @@ package com.lvfq.library.utils;
 
 import android.app.Activity;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Toast;
 
 /**
@@ -22,17 +23,67 @@ public class LvToastUtil {
      */
     private static Toast toast = null;
 
-    private static void showToast(final Activity context, final String message, final int gravity) {
+    public static Toast getToast() {
+        return toast;
+    }
+
+    /**
+     * 取消当前提示
+     */
+    public static void cancel() {
+        if (toast != null) {
+            toast.cancel();
+        }
+    }
+
+    /**
+     * 显示系统 Toast
+     *
+     * @param context 作用域
+     * @param message 显示信息
+     * @param gravity 显示位置
+     */
+    public static void showToast(final Activity context, final String message, final int gravity, final int duration) {
         if (context != null) {
             context.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     if (toast == null) {
-                        toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+                        toast = Toast.makeText(context, message, duration);
                     } else {
                         toast.setText(message);
                     }
                     if (gravity != 0) {
+                        toast.setGravity(gravity, 0, 0);
+                    }
+                    toast.show();
+                }
+            });
+        }
+    }
+
+    /**
+     * 显示自定义 Toast
+     *
+     * @param context 作用域
+     * @param view    自定义视图
+     * @param gravity 显示位置
+     */
+    public static void showToast(final Activity context, final View view, final int gravity, final int duration) {
+        if (context != null) {
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (toast == null) {
+                        toast = new Toast(context);
+                        toast.setView(view);
+                        toast.setDuration(duration);
+                    } else {
+                        toast.setView(view);
+                    }
+                    if (gravity == 0) {
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                    } else {
                         toast.setGravity(gravity, 0, 0);
                     }
                     toast.show();
@@ -48,7 +99,27 @@ public class LvToastUtil {
      * @param message
      */
     public static void showToast(Activity context, String message) {
-        showToast(context, message, 0);
+        showToast(context, message, 0, Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * normal show toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showToast(Activity context, int gravity, String message) {
+        showToast(context, message, gravity, Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * normal show toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showToast(Activity context, String message, int duration) {
+        showToast(context, message, 0, duration);
     }
 
     /**
@@ -59,6 +130,36 @@ public class LvToastUtil {
      */
     public static void showToastCenter(Activity activity, String message) {
         showToast(activity, message, Gravity.CENTER);
+    }
+
+    /**
+     * 显示自定义 Toast
+     *
+     * @param activity 作用域
+     * @param view     自定义视图
+     */
+    public static void showToast(Activity activity, View view) {
+        showToast(activity, view, 0, Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * 显示自定义 Toast
+     *
+     * @param activity 作用域
+     * @param view     自定义视图
+     */
+    public static void showToast(Activity activity, int gravity, View view) {
+        showToast(activity, view, gravity, Toast.LENGTH_SHORT);
+    }
+
+    /**
+     * 显示自定义 Toast
+     *
+     * @param activity 作用域
+     * @param view     自定义视图
+     */
+    public static void showToast(Activity activity, View view, int duration) {
+        showToast(activity, view, 0, duration);
     }
 
 }
