ee4d389cf89f63d6b38429bdaa326806bd419ce5
吕发强 <903019642@qq.com>
2017-08-16 14:06:57 +0800

    添加对比版本号方法。

--- library/src/main/java/com/lvfq/library/utils/LvAndroidUtil.java
+++ library/src/main/java/com/lvfq/library/utils/LvAndroidUtil.java
@@ -45,4 +45,41 @@ public class LvAndroidUtil {
         return "";
     }
 
+    /**
+     * 比较两个版本的大小
+     *
+     * @param version1
+     * @param version2
+     * @return 返回0和-1，-1表示小于后面的版本
+     */
+    public static int compareVersion(String version1, String version2) {
+        if (version1.equals(version2)) {
+            return 0;
+        }
+        String[] version1Array = version1.split("\\.");
+        String[] version2Array = version2.split("\\.");
+        int index = 0;
+        int minLen = Math.min(version1Array.length, version2Array.length);
+        int diff = 0;
+        while (index < minLen && (diff = Integer.parseInt(version1Array[index]) - Integer.parseInt(version2Array[index])) == 0) {
+            index++;
+        }
+        if (diff == 0) {
+            for (int i = index; i < version1Array.length; i++) {
+                if (Integer.parseInt(version1Array[i]) > 0) {
+                    return 1;
+                }
+            }
+            for (int i = index; i < version2Array.length; i++) {
+                if (Integer.parseInt(version2Array[i]) > 0) {
+                    return -1;
+                }
+            }
+
+            return 0;
+        } else {
+            return diff > 0 ? 1 : -1;
+        }
+    }
+
 }
